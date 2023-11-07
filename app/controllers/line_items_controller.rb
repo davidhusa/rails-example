bb90bf4997100e138160b33975da24e01561dd43
David Husa <davidhusa@gmail.com>
2023-11-07 00:32:49 -0800

    style totals. add filters

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,14 +2,25 @@ class LineItemsController < ApplicationController
   PAGE_SIZE = 250
 
   def index
-    @line_items = LineItem.includes(:campaign)
-    @max_page = LineItem.count.to_f / PAGE_SIZE
-    @page = params[:page].present? ? params[:page].to_i : 0
+    set_line_items
+    set_page(@line_items)
     set_grand_totals(@line_items, @page)
+
     @line_items_paginated = @line_items.offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
   end
 
   private
+  def set_line_items
+    @line_items = LineItem.joins(:campaign)
+    @line_items = @line_items.where("UPPER(line_items.name) like ?", "%#{params[:name_filter].upcase}%") if params[:name_filter].present?
+    @line_items = @line_items.where("UPPER(campaigns.name) like ?", "%#{params[:campaign_name_filter].upcase}%") if params[:campaign_name_filter].present?
+  end
+
+  def set_page(line_items)
+    @max_page = line_items.count.to_f / PAGE_SIZE
+    @page = params[:page].present? ? params[:page].to_i : 0
+  end
+
   def set_grand_totals(line_items, page)
     return unless page.zero?
 
