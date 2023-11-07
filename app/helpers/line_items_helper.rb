ff1d82a9a0640567712ec4be363f2e750155255f
David Husa <davidhusa@gmail.com>
2023-11-06 22:23:31 -0800

    remove unneeded pagination helpers

--- app/helpers/line_items_helper.rb
+++ app/helpers/line_items_helper.rb
@@ -1,14 +1,2 @@
 module LineItemsHelper
-  def previous_page_link(page)
-    change_page_link("Previous", page - 1)
-  end
-
-  def next_page_link(page)
-    change_page_link("Next", page + 1)
-  end
-
-  def change_page_link(text, page)
-    link_params = { page: page }
-    link_to text, line_items_path(params: link_params, rel: "next")
-  end
 end
